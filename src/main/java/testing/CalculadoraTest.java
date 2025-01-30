d43be770b05e13e70b56d3348f61ba0f2c2d6409
Polita86 <paula.arroyo.profe@gmail.com>
2025-01-30 11:56:50 +0100

    subiendo mi clae Cociente a mi rama

--- src/main/java/testing/CalculadoraTest.java
+++ src/main/java/testing/CalculadoraTest.java
@@ -1,6 +1,24 @@
 package testing;
 
+import javabean.Cociente;
+
 public class CalculadoraTest {
 
+    public static void main(String[] args) {
+        Calculadora calc = new Calculadora();
+
+        /**
+         *   Ejemplo de uso de los métodos
+          */
+
+        try {
+            System.out.println("División de reales: " + calc.dividirReales(20.5, 2.5));
+            System.out.println("División de enteros: " + calc.dividirEnteros(20, 2));
+            System.out.println("Inverso de 8.0: " + calc.inversoReal(8.0));
+            System.out.println("Raíz de 16: " + calc.raiz(16));
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
 
