69ba5df9b7ca79b0ccb69b972eccf95a4c2a8a9f
Kevin <kevin.crespo829@comunidadunir.net>
2025-01-30 12:09:24 +0100

    Segundo intento de subida del proyecto al repositorio remoto

--- src/main/java/javabean/Resta.java
+++ src/main/java/javabean/Resta.java
@@ -1,4 +1,127 @@
 package javabean;
 
+import java.util.Scanner;
+
 public class Resta {
+
+    // Atributos de instancia //
+    private int entero1;
+    private int entero2;
+    private double real1;
+    private double real2;
+    private double real3;
+    private double valorAcumulado;
+    private Scanner scanner = new Scanner(System.in);
+
+    // Constructor con todos los campos //
+    public Resta(int entero1,
+                 int entero2,
+                 double real1,
+                 double real2,
+                 double real3,
+                 double valorAcumulado,
+                 Scanner scanner) {
+        super();
+        this.entero1 = entero1;
+        this.entero2 = entero2;
+        this.real1 = real1;
+        this.real2 = real2;
+        this.real3 = real3;
+        this.valorAcumulado = valorAcumulado;
+        this.scanner = scanner;
+    }
+
+    // Constructor vacío //
+    public Resta() {
+        super();
+    }
+
+    // gets y sets //
+    public int getEntero1() {
+        return entero1;
+    }
+    public void setEntero1(int entero1) {
+        this.entero1 = entero1;
+    }
+    public int getEntero2() {
+        return entero2;
+    }
+    public void setEntero2(int entero2) {
+        this.entero2 = entero2;
+    }
+    public double getReal1() {
+        return real1;
+    }
+    public void setReal1(double real1) {
+        this.real1 = real1;
+    }
+    public double getReal2() {
+        return real2;
+    }
+    public void setReal2(double real2) {
+        this.real2 = real2;
+    }
+    public double getReal3() {
+        return real3;
+    }
+    public void setReal3(double real3) {
+        this.real3 = real3;
+    }
+    public double getValorAcumulado() {
+        return valorAcumulado;
+    }
+    public void setValorAcumulado(double valorAcumulado) {
+        this.valorAcumulado = valorAcumulado;
+    }
+    public Scanner getScanner() {
+        return scanner;
+    }
+    public void setScanner(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+
+    // Métodos propios //
+    public double restaDosReales(){
+        System.out.println("Vamos a restar dos números reales. ");
+        System.out.println("Introduce el primer número real: ");
+        real1 = scanner.nextDouble();
+
+        System.out.println("Introduce el segundo número real: ");
+        real2 = scanner.nextDouble();
+
+        valorAcumulado += (real1 - real2);
+
+        return real1 - real2;
+    }
+
+    public int restaDosEnteros() {
+        System.out.println("Vamos a restar dos números enteros. ");
+        System.out.println("Introduce el primer número entero: ");
+        entero1 = scanner.nextInt();
+
+        System.out.println("Introduce el segundo número entero: ");
+        entero2 = scanner.nextInt();
+
+        valorAcumulado += (entero1 - entero2);
+
+        return entero1 - entero2;
+
+    }
+
+    public double restaTresReales() {
+        System.out.println("Vamos a restar tres números reales. ");
+        System.out.println("Introduce el primer número real: ");
+        real1 = scanner.nextDouble();
+
+        System.out.println("Introduce el segundo número real: ");
+        real2 = scanner.nextDouble();
+
+        System.out.println("Introduce el tercer número real: ");
+        real3 = scanner.nextDouble();
+
+        valorAcumulado += (real1 - real2 - real3);
+
+        return real1 - real2 - real3;
+    }
 }
--- src/main/java/testing/CalculadoraTest.java
+++ src/main/java/testing/CalculadoraTest.java
@@ -1,6 +1,48 @@
 package testing;
 
-public class CalculadoraTest {
+import javabean.Suma;
+import javabean.Resta;
 
+
+public class CalculadoraTest {
+    public static void main(String[] args) {
+
+
+        // ---------------------- SUMA ------------------------ //
+        Suma suma1 = new Suma();
+
+        double resultado_suma1 = 	suma1.sumaDosReales();
+        int resultado_suma2    = 	suma1.sumaDosEnteros();
+        double resultado_suma3 = 	suma1.sumaTresReales();
+
+        System.out.println("El resultado es: " + resultado_suma1);
+        System.out.println("El resultado es: " + resultado_suma2);
+        System.out.println("El resultado es: " + resultado_suma3);
+
+        System.out.println(
+                "El valor acumulado de todas las operaciones es: "
+                        + suma1.getValorAcumulado());
+        // ---------------------------------------------------- //
+
+        System.out.println(" ");
+        System.out.println(" ");
+
+        // ---------------------- RESTA ----------------------- //
+        Resta resta1 = new Resta();
+        double resultado_resta1 = 	resta1.restaDosReales();
+        int resultado_resta2    = 	resta1.restaDosEnteros();
+        double resultado_resta3 = 	resta1.restaTresReales();
+
+        System.out.println("El resultado es: " + resultado_resta1);
+        System.out.println("El resultado es: " + resultado_resta2);
+        System.out.println("El resultado es: " + resultado_resta3);
+
+        System.out.println(
+                "El valor acumulado de todas las operaciones es: "
+                        + resta1.getValorAcumulado());
+        // ---------------------------------------------------- //
+
+    }
 }
+
 
