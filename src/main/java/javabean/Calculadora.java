8311a8ac37d767e2b7eeffede0c007a1c06010aa
ramsesmr <adrianramsesmunozrodriguez@gmail.com>
2025-02-02 16:45:08 +0100

    se ha intaciado la clase producto en la clase principal calculadora y se han escrito los metodos correspondiente para que la clase Producto funcione de manera correcta en la clase Calculadora, se han implementado comentarios de javaDoc

--- src/main/java/javabean/Calculadora.java
+++ src/main/java/javabean/Calculadora.java
@@ -1,39 +1,107 @@
 package javabean;
 
+/**
+ *<p>Clase Calculadora </p>
+ *
+ * <pre>Implementa 4 clases como atributos de intancias que son las que nos va a permiri hacer las operaciones correspondiente </pre>
+ *
+ * @see Producto
+ * @see Sumar
+ * @see Restar
+ * @see Dividir
+ *
+ *
+ * @author Paula Arroyo
+ * @author Kevin
+ * @author Adrian Muñoz Rodriguez
+ * @version 1.1
+ */
 
 public class Calculadora {
 
 //atributos de clases
-
+    /**
+     *
+     * @see Producto
+     * @see Sumar
+     * @see Restar
+     * @see Dividir
+     */
    double resultado = 0;
-
+    Producto producto = new Producto();
    //constructor
+
+
+    /**
+     * metodo constructor vacio
+     */
     public Calculadora() {
     }
 
     //getter
+
+    /**
+     *
+     * @return el valor actual de la variable {@code private} de tipo {@code double} resultado, que contiene el valor de la ultima operacion realizada
+     */
     public double getResultado() {
         return resultado;
     }
 
 
     //metodos propios
-    public double multiplicar(int a, int b) {
 
-        Producto producto = new Producto();
-        return  this.resultado=producto.multiplicar(a,b);
+    /**
+     * @author Adrian Ramses Muñoz Rodriguez
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+    public void multiplicar(int a, int b) {
+
+        this.resultado=producto.multiplicar(a,b);
+        System.out.println(resultado);
     }
 
-    public double multiplicar(int a, int b,int c) {
+    /**
+     * @author Adrian Ramses Muñoz Rodriguez
+     *
+     * @param a
+     * @param b
+     * @param c
+     * @return
+     */
+    public void multiplicar(int a, int b,int c) {
 
-        Producto producto = new Producto();
-        return  this.resultado=producto.multiplicar(a,b,c);
+      this.resultado=producto.multiplicar(a,b,c);
+        System.out.println(resultado);
     }
 
-    public double multiplicar(double a, double b) {
+    /**
+     * @author Adrian Ramses Muñoz Rodriguez
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+    public void multiplicar(double a, double b) {
 
-        Producto producto = new Producto();
-        return  this.resultado=producto.multiplicar(a,b);
+        this.resultado=producto.multiplicar(a,b);
+        System.out.println(resultado);
+    }
+
+    /**
+     * @author Adrian Ramses Muñoz Rodriguez
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+    public void potencia(int a, int b) {
+
+        this.resultado=producto.potencia(a,b);
+        System.out.println(resultado);
     }
 
 
@@ -42,8 +110,9 @@ public class Calculadora {
 
 
 Calculadora calculadora = new Calculadora();
-        calculadora.multiplicar(2.7,3.7);
-        System.out.println(calculadora.getResultado());
+
+        calculadora.potencia(2,3);
+        calculadora.multiplicar(2,3);
 
     }
 }
