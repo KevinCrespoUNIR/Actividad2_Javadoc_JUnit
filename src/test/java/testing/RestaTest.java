a0c2bc744e3d63f1e82ca826b8d1ee0c73a93a34
Kevin <kevin.crespo829@comunidadunir.net>
2025-02-05 13:01:53 +0100

    UPDATE: Creado test de Suma y Resta

---
+++ src/test/java/testing/RestaTest.java
@@ -0,0 +1,76 @@
+package testing;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RestaTest {
+
+    @Test
+    void getEntero1() {
+    }
+
+    @Test
+    void setEntero1() {
+    }
+
+    @Test
+    void getEntero2() {
+    }
+
+    @Test
+    void setEntero2() {
+    }
+
+    @Test
+    void getReal1() {
+    }
+
+    @Test
+    void setReal1() {
+    }
+
+    @Test
+    void getReal2() {
+    }
+
+    @Test
+    void setReal2() {
+    }
+
+    @Test
+    void getReal3() {
+    }
+
+    @Test
+    void setReal3() {
+    }
+
+    @Test
+    void getValorAcumulado() {
+    }
+
+    @Test
+    void setValorAcumulado() {
+    }
+
+    @Test
+    void getScanner() {
+    }
+
+    @Test
+    void setScanner() {
+    }
+
+    @Test
+    void restaDosReales() {
+    }
+
+    @Test
+    void restaDosEnteros() {
+    }
+
+    @Test
+    void restaTresReales() {
+    }
+}
