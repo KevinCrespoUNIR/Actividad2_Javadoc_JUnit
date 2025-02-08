2d5eaab8b2f616512f5c05ee152fcef2febc85df
ramsesmr <adrianramsesmunozrodriguez@gmail.com>
2025-02-08 17:34:31 +0100

    actualizando master con los cambios del test de cociente

--- src/test/java/testing/CocienteTest.java
+++ src/test/java/testing/CocienteTest.java
@@ -1,6 +1,6 @@
 package testing;
+import javabean.*;
 
-import javabean.Cociente;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
